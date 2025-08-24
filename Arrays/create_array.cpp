bf74fec68769fb171d14184eb71cc35ff19fff50
Muhammad Younas Peerzada <pakistan14612@gmail.com>
2025-08-24 09:46:34 +0500

    Update create_array.cpp

--- Arrays/create_array.cpp
+++ Arrays/create_array.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    int price[5];          // Declares an array of size 5
+    int price[5];         // Declares an array of size 5
     int marks[5] = {45, 67, 90, 65, 76};  // Array initialization with explicit size
     int score[] = {76, 65, 98, 31, 29};   // Array initialization with implicit size.
     int num[4] = {4, 5};  // Here the memory is allocated for the 2 items.The
