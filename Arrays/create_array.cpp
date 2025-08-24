fd5220a7feb00f729a29352f03978e369b85811c
Muhammad Younas Peerzada <pakistan14612@gmail.com>
2025-08-24 09:47:04 +0500

    Create create_array.cpp

---
+++ Arrays/create_array.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int price[5];          // Declares an array of size 5
+    int marks[5] = {45, 67, 90, 65, 76};  // Array initialization with explicit size
+    int score[] = {76, 65, 98, 31, 29};   // Array initialization with implicit size.
+    int num[4] = {4, 5};  // Here the memory is allocated for the 2 items.The
+                          // remaining last 2 elements is initialized to zero.
+
+    cout << marks[0] << endl;
+    cout << marks[1] << endl;
+    cout << marks[2] << endl;
+    cout << marks[3] << endl;
+    cout << marks[4] << endl;
+
+    return 0;
+}
