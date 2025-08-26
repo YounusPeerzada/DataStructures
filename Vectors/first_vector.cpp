58b00593ac0a4e85079879242484a980c5b6c7aa
Muhammad Younas Peerzada <pakistan14612@gmail.com>
2025-08-26 10:38:13 +0500

    Create first_vector.cpp

---
+++ Vectors/first_vector.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<int> vec = {54, 89, 80, 87, 86};
+
+    cout << vec[0] << endl;
+    cout << vec[1] << endl;
+    cout << vec[2] << endl;
+    cout << vec[3] << endl;
+    cout << vec[4] << endl;
+
+    return 0;
+}
