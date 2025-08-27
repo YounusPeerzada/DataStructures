d98ed755f6aed1c0fc63fb3f334011b22324171e
Muhammad Younas Peerzada <pakistan14612@gmail.com>
2025-08-27 12:46:36 +0500

    Update at_function.cpp

--- Vectors/at_function.cpp
+++ Vectors/at_function.cpp
@@ -5,9 +5,10 @@ using namespace std;
 
 int main() {
     vector<int> distance = {90, 89, 67, 56, 78, 89, 98};
+    int size = 7;
 
-    for (int i : distance) {
-        cout << i << endl;
+    for (int i = 0; i < size; i++) {
+        cout << distance.at(i) << endl;  //  Alternative to the subscript operator [] for accessing elements
     }
 
     return 0;
