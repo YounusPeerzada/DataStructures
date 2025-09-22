16420b87686dfd6545ed2dcd5afee09e293bc6b7
Muhammad Younas Peerzada <pakistan14612@gmail.com>
2025-09-22 21:51:09 +0500

    Create loop_on_2D_array.cpp

---
+++ 2D Arrays/loop_on_2D_array.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5][5] = {
+        {1, 2, 3, 4, 5},
+        {6, 7, 8, 9, 10},         
+        {11, 12, 13, 14, 15},
+        {16, 17, 18, 19, 20},
+        {21, 22, 23, 24, 25}
+    };
+
+    int rows = 5;
+    int cols = 5;
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
